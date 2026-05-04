459eeb3d5bb8887fed09d0c6894de9480cc78e9c
CreativityEngr <adkfafutuka@gmail.com>
2026-05-04 20:28:08 +0100

    add transaction history with etherscan v2 integration

--- server/routes/wallet.js
+++ server/routes/wallet.js
@@ -15,6 +15,22 @@ router.get("/:address", async (req, res) => {
    const balance = await provider.getBalance(address);
 const ethBalance = ethers.formatEther(balance);
 
+let txs = [];
+
+const ETHERSCAN_API_KEY = "Q39DIBNY5K2Q98KQS1TD4XKCXWKD18C8PX";
+
+try {
+const historyRes = await axios.get(
+  `https://api.etherscan.io/v2/api?chainid=1&module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=desc&apikey=${ETHERSCAN_API_KEY}`
+);
+
+  if (Array.isArray(historyRes.data.result)) {
+    txs = historyRes.data.result.slice(0, 5);
+  }
+} catch (err) {
+  console.log("tx fetch error:", err.message);
+}
+
 // get ETH price (USD)
 const priceRes = await axios.get(
   "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
@@ -28,7 +44,8 @@ const usdValue = (parseFloat(ethBalance) * ethPrice).toFixed(2);
 res.json({
   address,
   balance: ethBalance,
-  usd: usdValue
+  usd: usdValue,
+  txs
 });
 
   } catch (err) {
