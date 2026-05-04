5d712f80e65be2738e3fa3a18a12e573e5e7ca73
CreativityEngr <adkfafutuka@gmail.com>
2026-05-04 19:26:12 +0100

    add USD conversion and full frontend integration

--- server/routes/wallet.js
+++ server/routes/wallet.js
@@ -1,3 +1,5 @@
+const axios = require("axios");
+
 const express = require("express");
 const router = express.Router();
 const { ethers } = require("ethers");
@@ -10,13 +12,24 @@ router.get("/:address", async (req, res) => {
   try {
     const { address } = req.params;
 
-    const balance = await provider.getBalance(address);
-    const ethBalance = ethers.formatEther(balance);
+   const balance = await provider.getBalance(address);
+const ethBalance = ethers.formatEther(balance);
 
-    res.json({
-      address,
-      balance: ethBalance
-    });
+// get ETH price (USD)
+const priceRes = await axios.get(
+  "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
+);
+
+const ethPrice = priceRes.data.ethereum.usd;
+
+// calculate USD value
+const usdValue = (parseFloat(ethBalance) * ethPrice).toFixed(2);
+
+res.json({
+  address,
+  balance: ethBalance,
+  usd: usdValue
+});
 
   } catch (err) {
     console.log(err);
